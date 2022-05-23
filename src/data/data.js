bf1cbaba9f8b344cf8db3834e13f7aa10b3ffdbc
edjunma <edjunma@gmail.com>
2022-05-23 01:32:29 -0400

    5/23 minor update

--- src/data/data.js
+++ src/data/data.js
@@ -17,7 +17,7 @@ export const heroData = {
   title: "Hello there ✌🏼, I'm ",
   name: 'Edmond',
   subtitle: 'A ',
-  cta: 'Click Me!',
+  cta: 'Click Me',
 };
 
 // ABOUT DATA
@@ -66,7 +66,7 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: 'Please feel free to send me an email or connect with me on my social media links below. I will get back to you as soon as possible with some 🍵 and 🍪.',
-  btn: "Let's Talk Business! 🤝🏼",
+  btn: "Let's Talk Business 🤝🏼",
   email: 'edjunma@gmail.com',
 };
 
